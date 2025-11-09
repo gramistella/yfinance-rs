1c22dddd24d373e2ab2b9fd28d5e5bc9860a7504
G. Ramistella <60039721+gramistella@users.noreply.github.com>
2025-11-09 20:29:01 +0100

    test: disable esg tests as endpoint is no longer available

--- tests/esg.rs
+++ tests/esg.rs
@@ -1,10 +1,12 @@
 mod common;
 
-#[path = "esg/live.rs"]
-mod esg_live;
-#[path = "esg/live_involvement.rs"]
-mod esg_live_involvement;
-#[path = "esg/offline.rs"]
-mod esg_offline;
-#[path = "esg/offline_involvement.rs"]
-mod esg_offline_involvement;
+// These are disabled because the endpoint is not available anymore.
+// These might be able to be enabled again if we can find a way to get the data.
+// #[path = "esg/live.rs"]
+// mod esg_live;
+// #[path = "esg/live_involvement.rs"]
+// mod esg_live_involvement;
+// #[path = "esg/offline.rs"]
+// mod esg_offline;
+// #[path = "esg/offline_involvement.rs"]
+// mod esg_offline_involvement;
